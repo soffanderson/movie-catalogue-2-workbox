03232c6d8d2ca03d9a7d0ecc931c599644e7816f
soffanderson <s.btudz@gmail.com>
2023-05-24 22:56:06 +0800

    Selesai membuat Service Worker dengan Workbox

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: 'fd8f2de6a01f4f6f3e2b5692cc6d36cc',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+};
+
+export default CONFIG;
