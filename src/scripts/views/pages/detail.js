06b090edbcb3976388c98ccb5919e83b5c29f0d4
soffanderson <s.btudz@gmail.com>
2023-05-28 22:34:35 +0800

    Membuat tombol like (belum berfungsi)

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,15 @@
 import TheMovieDbSource from '../../data/themoviedb-source';
 import UrlParser from '../../routes/url-parser';
-import { createMovieDetailTemplate } from '../templates/template-creator';
+import {
+  createLikeButtonTemplate,
+  createMovieDetailTemplate,
+} from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
     <div id="movie" class="movie"></div>
+    <div id="createLikeButtonTemplate"></div>
     `;
   },
 
@@ -14,6 +18,7 @@ const Detail = {
     const movie = await TheMovieDbSource.detailMovie(url.id);
     const movieContainer = document.querySelector('#movie');
     movieContainer.innerHTML = createMovieDetailTemplate(movie);
+    createLikeButtonTemplate.innerHTML = createLikeButtonTemplate();
   },
 };
 
